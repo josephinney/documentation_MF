b0b8ca6f16138dd9bd8c8cae5bb8948b8abd58ec
Jose Emilio <josemilio.1995@gmail.com>
2025-02-21 18:35:17 +0300

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
+import './Documentation'
+import Documentation from './Documentation';
 
 const App = () => {
   return (
     <div className="content">
       <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+      <h2>The documentation below will be exported as module.</h2>
+      <h2>--------------------------------------------------------</h2>
+      <Documentation/>
     </div>
   );
 };
